ff5171d5d026b4ad43461347ae229c0e62101252
Kyanari <11563770+xkyanari@users.noreply.github.com>
2023-04-06 18:30:47 -0700

    Update index.js
    Signed-off-by: Kyanari <11563770+xkyanari@users.noreply.github.com>

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const getBlockCount = require('./v1/getBlockCount');
 const ls = require('./v1/ls'); // Import the ls function
 
-router.post('/getBlockCount', getBlockCount);
+router.get('/getBlockCount', getBlockCount);
 router.get('/ls', ls); // Add a route for the ls function
 
 module.exports = router;
